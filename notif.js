ab082df88c07b44269fb275ad484f74001763938
mikiko <mikikotech@gmail.com>
2023-06-08 23:49:10 +0800

    notif

--- notif.js
+++ notif.js
@@ -8,45 +8,56 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
 
-route.get("/", async (req, res) => {
+route.post("/:id", async (req, res) => {
     // console.log("notif body = ", req.body);
+    // console.log('notif param = ', req.params.id);
 
     try {
 
-        var data = admin.firestore()
-            .collection('fcmToken')
-            .doc('fcmToken')
+        admin.firestore()
+            .collection('patient')
+            .where('sensor_id', '==', req.params.id)
             .get()
-            .then((data) => {
-                console.log(data.data());
+            .then((resp) => {
+                // console.log('pasien id = ', resp.docs[0].data());
 
-                var fcm_token = data.data().fcm_token
+                var data = admin.firestore()
+                    .collection('fcmToken')
+                    .doc('fcmToken')
+                    .get()
+                    .then((data) => {
+                        // console.log(data.data());
 
-                if (data.data() != undefined) {
-                    admin.messaging()
-                        .sendMulticast(
-                            {
-                                data: {
-                                    message: 'urgent notification'
-                                },
-                                tokens: fcm_token,
-                                notification: {
-                                    title: 'urgent notification',
-                                    body: 'suhu pasien tinggi'
-                                }
-                            }
-                        )
-                        .then((resp) => {
-                            console.log(resp);
-                            if (resp.responses[0].success) {
-                                res.send('notif')
-                            } else {
-                                res.send('failed')
-                            }
-                        })
-                }
+                        var fcm_token = data.data().fcm_token
+
+                        if (data.data() != undefined) {
+                            admin.messaging()
+                                .sendMulticast(
+                                    {
+                                        data: {
+                                            id: resp.docs[0].data().id,
+                                            patient_name: resp.docs[0].data().name
+                                        },
+                                        tokens: fcm_token,
+                                        notification: {
+                                            title: 'urgent notification',
+                                            body: `${req.body.data} ${req.body.condition == '>' ? 'diatas' : 'dibawah'} ${req.body.value} ${req.body.data == 'suhu' ? 'C' : req.body.data == 'oxygen' ? '%' : req.body.data == 'blood' ? 'mmHg' : 'bpm'}`
+                                        }
+                                    }
+                                )
+                                .then((resp) => {
+                                    console.log(resp);
+                                    if (resp.responses[0].success) {
+                                        res.send('notif')
+                                    } else {
+                                        res.send('failed')
+                                    }
+                                })
+                        }
+                    })
+                    .catch((e) => { console.log(e); })
+
             })
-            .catch((e) => { console.log(e); })
     } catch (error) {
         res.status(400).send("error");
     }
